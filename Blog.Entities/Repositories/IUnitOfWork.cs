0392f085cc30435ec9d7e553419f244b07f0ef06
Ahmed <ahmedsamylike@gmail.com>
2024-09-15 20:08:24 +0300

    first commit

---
+++ Blog.Entities/Repositories/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Entities.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IApplicationUserRepository ApplicationUser { get; }   
+        ICommentRepository Comment { get; }
+        ICategoryRepository Category { get; }
+        IPostRepository Post { get; }
+        int Complete();
+    }
+}
---
+++ Blog.Web/Areas/Visitor/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Entities.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Visitor.Controllers
+{
+    [Area("Visitor")]
+
+    public class CategoryController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var categories = _unitOfWork.Category.GetAll();
+            return View(categories);
+        }
+
+
+        public IActionResult GetCategory(int? id)
+        {
+            if (id == null || id <= 0)
+            {
+                return NotFound();
+            }
+
+            var category = _unitOfWork.Category.GetFirstorDefault(
+                x => x.Id == id,
+                Includeword: "Posts"
+            );
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+    }
+}
